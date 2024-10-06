7295f8997625eee7a10c4878c93982840d2726a1
Tomasz Sas <tomaszsas.dev@gmail.com>
2024-10-06 08:54:52 +0200

    Add state management to PersonScore component
    Implemented useState to manage name, score, and loading states. Display loading message until person data is fetched, and added buttons to increment, decrement, and reset the score.

--- src/PersonScore.tsx
+++ src/PersonScore.tsx
@@ -1,13 +1,31 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getPerson } from './getPerson';
 
 export function PersonScore() {
+  const [name, setName] = useState<string | undefined>();
+  const [score, setScore] = useState(0);
+  const [loading, setLoading] = useState(true);
+
   useEffect(() => {
     async function getThePerson() {
       const person = await getPerson();
-      console.log(person);
+      setLoading(false);
+      setName(person.name);
     }
+
     getThePerson();
   }, []);
-  return null;
+  if (loading) {
+    return <div>Ładowanie...</div>;
+  }
+  return (
+    <div>
+      <h3>
+        {name}, {score}
+      </h3>
+      <button onClick={() => setScore(score + 1)}>Dodaj</button>
+      <button onClick={() => setScore(score - 1)}>Odejmij</button>
+      <button onClick={() => setScore(0)}>Zresetuj</button>
+    </div>
+  );
 }
