bf2a549164a90a5222382dd7b781a3b1ff500f1e
Tomasz Sas <tomaszsas.dev@gmail.com>
2024-10-06 08:30:59 +0200

    Modify function PersonScore to use async/await syntax

--- src/PersonScore.tsx
+++ src/PersonScore.tsx
@@ -3,9 +3,11 @@ import { getPerson } from './getPerson';
 
 export function PersonScore() {
   useEffect(() => {
-    getPerson().then((person) => {
+    async function getThePerson() {
+      const person = await getPerson();
       console.log(person);
-    });
+    }
+    getThePerson();
   }, []);
   return null;
 }
